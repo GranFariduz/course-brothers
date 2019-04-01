1dd2f843007f12c082fa485bbacae7f92e03eef9
Farid <relampagolanzadel04@gmail.com>
2019-04-01 23:11:51 +0530

    Added news and updates page

---
+++ pages/news-updates.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="..\assets\images\favicon.png" type="image/png" />
+
+  <!-- NORMALIZE CSS -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
+
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700" rel="stylesheet">
+
+  <!-- FONTAWESOME -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- ANIMATE CSS -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css' />
+
+  <!-- CUSTOM CSS -->
+  <link rel='stylesheet' href='../styles/style.css' />
+
+  <title>CourseBrother.com | Legal Policy</title>
+</head>
+<body>
+  
+  
+  <!-- MAIN_CONTAINER -->
+  <div class="main-container">
+
+
+    <!-- NAVBAR_SCROLL -->
+    <?php include '../includes/navbar-main.php' ?>
+    <!-- /NAVBAR_SCROLL -->
+
+    <!-- LOGIN_REGISTER_MODALS -->
+    <?php include '../includes/login-register-modal.php' ?>
+    <!-- /LOGIN_REGISTER_MODALS -->
+
+    <!-- NEWS_EVENTS -->
+    <div class="news_events">
+
+      <div class="news_events__header">
+        <h3 class='news_events__header__title'>news and events</h3>
+      </div>
+
+      <div class="news_events__main">
+        <div class="news_events__main__head">
+          <h4>assam downtown university announces new pgdm enrollment parnter</h4>
+          <h6>30th february, 2019</h6>
+        </div>
+        <div class="news_events__main__share">
+          share update &nbsp;
+          <i class='fas fa-share'></i>
+        </div>
+        <div class="news_events__main__content">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa culpa porro eveniet nostrum! Dolorem facilis, asperiores quos voluptate quia enim rem adipisci veniam. Sint rem sequi dicta veritatis reprehenderit aut?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa culpa porro eveniet nostrum! Dolorem facilis, asperiores quos voluptate quia enim rem adipisci veniam. Sint rem sequi dicta veritatis reprehenderit aut?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa culpa porro eveniet nostrum! Dolorem facilis, asperiores quos voluptate quia enim rem adipisci veniam. Sint rem sequi dicta veritatis reprehenderit aut?
+          </p>
+        </div>
+      </div>
+
+      <div class="news_events__more_updates">
+        <h4 class="news_events__more_updates__title">more updates</h4>
+        <div class="news_events__more_updates__container">
+          <div class="news_events__more_updates__item">
+            <h4>19th january, 2019</h4>
+            <h3>announcing our long lasting relationship with paytm</h3>
+            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, accusamus non quidem praesentium veritatis quam expedita quis excepturi sunt ullam?</h5>
+          </div>
+          <div class="news_events__more_updates__item">
+            <h4>19th january, 2019</h4>
+            <h3>announcing our long lasting relationship with paytm</h3>
+            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, accusamus non quidem praesentium veritatis quam expedita quis excepturi sunt ullam?</h5>
+          </div>
+          <div class="news_events__more_updates__item">
+            <h4>19th january, 2019</h4>
+            <h3>announcing our long lasting relationship with paytm</h3>
+            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, accusamus non quidem praesentium veritatis quam expedita quis excepturi sunt ullam?</h5>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /NEWS_EVENTS -->
+
+    <!-- FOOTER -->
+    <?php include '../includes/footer.php' ?>
+    <!-- /FOOTER -->
+
+  </div>
+  <!-- /MAIN_CONTAINER -->
+
+
+  <!-- jQuery -->
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous">
+  </script>
+  <!-- TweenMax -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+  <!-- CUSTOM JS -->
+  <script src='../js/navbar.js'></script>
+
+</body>
+</html> 
